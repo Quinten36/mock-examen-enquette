186f99b101d81c5ad10bdb555d0805c8d9c37782
Quinten36 <q.kempers@mcw.nl>
2021-01-16 13:50:13 +0100

    lichte form beveiliging

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
+<!-- <?php 
+//controller eerst de verbinding
+require 'config.inc.php';
+require 'php/functions.php';
+require 'php/logic.php'; 
+// require_once 'session.inc.php';
+?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GLR enquette</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 </head>
 <body>
-  
+  <h1>GLR enquette site</h1>
+  <div id="splashScreen">
+    <div>
+      <a href="#" id="signUpBtn">Sign up</a>
+      <a href="#" id="loginBtn">Login in</a>
+    </div>
+  </div>
+  <div id="formsScreen">
+    <div>
+      <form action="" id="signUpForm">
+        <fieldset>
+          <legend>Sign up</legend>
+          <input type="hidden" id="uuid4" name="uuid4">
+          <label for="">Studentnummer: </label><input type="number" id="stNummerInput" placeholder="Vul hier uw studentnummer in">
+          <label for="">Klas: </label><input type="text" id="klasInput" placeholder="Vul hier uw klas in">
+          <label for="">Naam: </label><input type="text" id="naamInput" placeholder="Vul hier uw naam in">
+          <label for="">Adres: </label><input type="text" id="adresInput" placeholder="Vul hier uw adres in">
+          <label for="">Postcode: </label><input type="text" id="postcodeInput" placeholder="Vul hier uw postcode in">
+          <label for="">Woonplaats: </label><input type="text" id="woonplaatsInput" placeholder="Vul hier uw woonplaats in">
+          <label for="">Leeftijd: </label><input type="number" id="leeftijdInput" placeholder="Vul hier uw leeftijd in">
+          <label for="">Email: </label><input type="email" id="emailInput" placeholder="Vul hier uw email in">
+          <input type="submit" value="Verzenden" id="submitSignUp">
+        </fieldset>
+      </form>
+      <div id="emailSendPopUp">
+        <h3>Er is een email verstuurd naar: <span id="emailAdresSend"></span></h3>
+        Hierin vindt u een link voor het afronden van de regisstratie
+      </div>
+    </div>
+    <div>
+      <form action="" id="loginForm">
+        <fieldset>
+          <legend>Login</legend>
+            <input type="hidden" id="uuid4" name="uuid4">
+            <label for="emailInput">Email: </label><input type="text" id="emailInput">
+            <label for="passwordInput">Password: </label><input type="password" id="passwordInput">
+            <input type="submit" value="Verzenden" id="submitLogin">
+        </fieldset>
+      </form>
+    </div>
+    <a href="#" id="backBtn">Go back</a>
+  </div>
+  <script src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
-$('a').on('click', function() {
-  console.log(this)
+function logger (message, color) {
+  console.log('%c'+message, 'background: #222; color: '+color)
+}
+
+$('a').on('click', function (e) {
+  let choiceFrom = this.id.split('B')[0];
+  if (choiceFrom == 'signUp') {
+    logger('going to signUp', 'orange');
+    $('#submitSignUp').attr("disabled", true);
+    document.getElementById('submitSignUp').addEventListener('click', (e) => { e.preventDefault(); })
+    $('#splashScreen').hide(); $('#formsScreen').show(); $('#signUpForm').show();
+  } else if (choiceFrom == 'login') {
+    logger('going to login', 'orange');
+    $('#submitLogin').attr("disabled", true);
+    document.getElementById('submitLogin').addEventListener('click', (e) => { e.preventDefault(); })
+    $('#splashScreen').hide(); $('#formsScreen').show(); $('#loginForm').show();
+
+  } else if (choiceFrom == 'back') {
+    logger('going to splash screen', 'orange');
+    $('#splashScreen').show(); $('#formsScreen').hide(); $('#loginForm').hide(); $('#signUpForm').hide();
+  } else {
+    logger('mystical error', 'red')
+  }
 })
+
+let fieldValues = [1,0,0,0,0,0,0,0];
+function checkFieldsInput(field, form) {
+  let fields = ['stNummer','klas','naam','adres','postcode','woonplaats','leeftijd','email'];
+
+  for (var i = 0; i < fields.length; i++) {
+    if (field == fields[i]) {
+      fieldValues[i] = 1;
+    }
+  }
+
+  if (fieldValues.includes(0)) {
+    console.log(fieldValues);
+    console.log('nog een 0 gevonden')
+  } else {
+    console.log('nothing found')
+    $('#submit'+form).attr("disabled", false);
+  }
+}
+
+$('#signUpForm>fieldset>input').on('input', function() {
+  checkFieldsInput(this.id.split('I')[0], 'SignUp')
+})
+
+$('#loginForm>fieldset>input').on('input', function() {
+  if (this.id.split('I')[0] == 'password') {
+    $('#submitLogin').attr("disabled", false);
+  }
+})
+
+$('#loginForm>fieldset>input').keyup(function(e) {
+  switch (e.keyCode) {
+     case 8: // Backspace
+     console.log('backspace')
+      if ($('#passwordInput')[0].value == "") {
+        $('#submitLogin').attr("disabled", true);
+      }
+        break;
+  }
+});

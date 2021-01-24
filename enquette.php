1b914a85bf212044466df83d23ecbb3e20328314
Quinten36 <q.kempers@mcw.nl>
2021-01-24 14:16:34 +0100

    begin enquette site

---
+++ enquette.php
@@ -0,0 +1,17 @@
+<?php 
+//checken of je bent ingelogd
+//checken welk level sec je heb
+  //misschien preloader while level check?
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Enquette</title>
+</head>
+<body>
+  <h1>Dit is de enquette</h1>
+</body>
+</html>

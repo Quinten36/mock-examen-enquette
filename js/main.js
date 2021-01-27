1b040814bd0d19d79277f62c8ee43a7cd74738f9
Quinten36 <q.kempers@mcw.nl>
2021-01-27 14:45:46 +0100

    cookie check

--- js/main.js
+++ js/main.js
@@ -62,3 +62,36 @@ $('#loginForm>fieldset>input').keyup(function(e) {
         break;
   }
 });
+
+/*************************************************************************************** CHECK FOR JUST REGISTER *******************/
+var myVar;
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function myFunction() {
+  myVar = setInterval(alertFunc, 300);
+}
+
+function alertFunc() {
+  if (getCookie('uuid').length > 2) {
+    console.log(getCookie('uuid'));
+    $('#splashScreen').hide();
+    $('#redirectScreen').show();
+  } 
+}
+
+myFunction();
